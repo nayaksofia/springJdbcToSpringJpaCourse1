650d8c8c50fd8f52e0f56bee5c6130cb2227ef72
nayaksofia <nayaksofia1989@gmail.com>
2024-05-02 00:56:51 +0530

    Work on JPA

--- src/main/java/com/sofi/springboot/learnjdbcjpa/course/jpa/CourseJpaRepository.java
+++ src/main/java/com/sofi/springboot/learnjdbcjpa/course/jpa/CourseJpaRepository.java
@@ -1,5 +1,41 @@
 package com.sofi.springboot.learnjdbcjpa.course.jpa;
 
+import org.springframework.stereotype.Repository;
+
+import com.sofi.springboot.learnjdbcjpa.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
 public class CourseJpaRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager; // Manage the entities
+
+	// Insert Method: Inserting Rows
+	public void insert(Course jpacourse) {
+		entityManager.merge(jpacourse);
+	}
+
+	// Find Method : Select By Id
+	public Course findById(long Id) {
+		return entityManager.find(Course.class, Id);
+	}
+
+	// Delete Method : Delete By Id
+	public void deleteById(long Id) {
+		Course jp_course = entityManager.find(Course.class, Id);
+		entityManager.remove(jp_course);
+	}
 
 }
+
+/*
+ * Note: SQL QUERIES are replaced and managed by EntityManager in JPA.
+ * Therefore, not to worries about SQL QUERIES in JPA.
+ * 
+ */
+
