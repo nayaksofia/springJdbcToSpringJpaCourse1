d1b54c4b30ecd3b364346ff3603ef0fa564f4d6e
nayaksofia <nayaksofia1989@gmail.com>
2024-05-02 00:43:27 +0530

    Work on Spring JDBC

--- src/main/java/com/sofi/springboot/learnjdbcjpa/course/CourseCommandLineRunner.java
+++ src/main/java/com/sofi/springboot/learnjdbcjpa/course/CourseCommandLineRunner.java
@@ -4,43 +4,68 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.sofi.springboot.learnjdbcjpa.course.jdbc.CourseSpringJdbcRepository;
 import com.sofi.springboot.learnjdbcjpa.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
 	
-	@Autowired  //For Spring JPA
-	private CourseSpringDataJpaRepository  repo;
+   @Autowired  //1.For Spring JDBC
+   private CourseSpringJdbcRepository repo;
+	
+	
+	
+	
+  
+//	@Autowired  //3.For Spring JPA
+//	private CourseSpringDataJpaRepository  repo;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-	    //To Insert Data:We Use save() For SpringJPA Data Entry
-		repo.save(new Course(1,"Learn Spring JPA --SpringJPA","In28Minutes"));
-		repo.save(new Course(2,"Learn  JPA --SpringJPA","In28Minutes"));
-		repo.save(new Course(3,"Learn Spring JDBC --SpringJPA","In28Minutes"));
-		repo.save(new Course(4,"Learn JDBC --SpringJPA","In28Minutes"));
-		repo.save(new Course(5,"Learn Hibernate --SpringJPA","In28Minutes"));
-		
-		//To Delete Data: We Use Long Id For SpringJPA
-		repo.deleteById(1l);
-		
-		//To Find Data By ID: We use Long Id For Spring JPA
-		System.out.println(repo.findById(2l));
-		System.out.println(repo.findById(3l));
-		
-     	System.out.println(repo.count());
-		
-		// Retrieving and printing all courses
-        System.out.println("All Courses:");
-        repo.findAll().forEach(System.out::println);
+//--------------------------For Spring JDBC --------------------------------
+		//To Insert Data
+		repo.insert(new Course(1,"Learn Spring JPA --SpringJDBC","In28Minutes"));
+		repo.insert(new Course(2,"Learn  JPA --SpringJDBC","In28Minutes"));
+		repo.insert(new Course(3,"Learn Spring JDBC --SpringJDBC","In28Minutes"));
+		repo.insert(new Course(4,"Learn JDBC --SpringJDBC","In28Minutes"));
+	    repo.insert(new Course(5,"Learn Hibernate --SpringJDBC","In28Minutes"));
         
-        //Creating By Custom Method
-        System.out.println(repo.findByAuthor("In28Minutes"));
-        System.out.println(repo.findByAuthor(""));
-       
-        System.out.println(repo.findByName("Learn JDBC --SpringJPA"));
-        System.out.println(repo.findByName("Learn Hibernate --SpringJPA"));
+	    //To Delete Data By Id
+	    repo.deleteById(1);
+	    
+	    //To Find Data By Id
+	    System.out.println(repo.findById(2));
+		System.out.println(repo.findById(3));
+		
+		
+//-----------------------------For Spring Data JPA ----------------------------
+//		//To Insert Data:We Use save() For SpringJPA Data Entry
+//		repo.save(new Course(1,"Learn Spring JPA --SpringJPA","In28Minutes"));
+//		repo.save(new Course(2,"Learn  JPA --SpringJPA","In28Minutes"));
+//		repo.save(new Course(3,"Learn Spring JDBC --SpringJPA","In28Minutes"));
+//		repo.save(new Course(4,"Learn JDBC --SpringJPA","In28Minutes"));
+//		repo.save(new Course(5,"Learn Hibernate --SpringJPA","In28Minutes"));
+//		
+//		//To Delete Data: We Use Long Id For SpringJPA
+//		repo.deleteById(1l);
+//		
+//		//To Find Data By ID: We use Long Id For Spring JPA
+//		System.out.println(repo.findById(2l));
+//		System.out.println(repo.findById(3l));
+//		
+//     	System.out.println(repo.count());
+//		
+//		// Retrieving and printing all courses
+//        System.out.println("All Courses:");
+//        repo.findAll().forEach(System.out::println);
+//        
+//        //Creating By Custom Method
+//        System.out.println(repo.findByAuthor("In28Minutes"));
+//        System.out.println(repo.findByAuthor(""));
+//       
+//        System.out.println(repo.findByName("Learn JDBC --SpringJPA"));
+//        System.out.println(repo.findByName("Learn Hibernate --SpringJPA"));
 		
 	}
 
--- src/main/java/com/sofi/springboot/learnjdbcjpa/course/jdbc/CourseSpringJdbcRepository.java
+++ src/main/java/com/sofi/springboot/learnjdbcjpa/course/jdbc/CourseSpringJdbcRepository.java
@@ -1,5 +1,51 @@
 package com.sofi.springboot.learnjdbcjpa.course.jdbc;
 
-public class CourseSpringJdbcRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import com.sofi.springboot.learnjdbcjpa.course.Course;
+
+@Repository
+public class CourseSpringJdbcRepository {
+	
+	@Autowired
+	private JdbcTemplate springJdbcTemplate;
+
+	//SQL QUERIES
+	private static String INSERT_QUERY = """
+			  insert into course (id, name, author) values (?,?,?)
+			""";
+
+	private static String DELETE_QUERY = """
+			  delete from course where id = ?;
+			""";
+
+	private static String SELECT_QUERY = """
+			  select * from course where id = ?
+			""";
+
+	// Method For Insertion
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+
+	// Method For Deletion
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
+	}
+
+	// Method For Selection
+	public Course findById(long id) {
+		return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
+
+	}
 }
+
+/*
+ * Note: -------- JDBC Vs. Spring JDBC--------------------
+ * IN JDBC: We write lot of SQL queries and lot of java code. IN
+ * SPRING JDBC : We write a lot of SQL queries and less java code. [SPRING JDBC
+ * Is Better Than JDBC]
+ */
