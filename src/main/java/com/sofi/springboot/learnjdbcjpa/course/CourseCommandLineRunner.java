91909a770a3b05cc522db9fefe4368bf3d6f3c53
nayaksofia <nayaksofia1989@gmail.com>
2024-05-02 00:14:54 +0530

    Custom Methods: Spring Data JPA

--- src/main/java/com/sofi/springboot/learnjdbcjpa/course/CourseCommandLineRunner.java
+++ src/main/java/com/sofi/springboot/learnjdbcjpa/course/CourseCommandLineRunner.java
@@ -1,5 +1,47 @@
 package com.sofi.springboot.learnjdbcjpa.course;
 
-public class CourseCommandLineRunner {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.sofi.springboot.learnjdbcjpa.course.springdatajpa.CourseSpringDataJpaRepository;
+
+@Component
+public class CourseCommandLineRunner implements CommandLineRunner{
+	
+	@Autowired  //For Spring JPA
+	private CourseSpringDataJpaRepository  repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	    //To Insert Data:We Use save() For SpringJPA Data Entry
+		repo.save(new Course(1,"Learn Spring JPA --SpringJPA","In28Minutes"));
+		repo.save(new Course(2,"Learn  JPA --SpringJPA","In28Minutes"));
+		repo.save(new Course(3,"Learn Spring JDBC --SpringJPA","In28Minutes"));
+		repo.save(new Course(4,"Learn JDBC --SpringJPA","In28Minutes"));
+		repo.save(new Course(5,"Learn Hibernate --SpringJPA","In28Minutes"));
+		
+		//To Delete Data: We Use Long Id For SpringJPA
+		repo.deleteById(1l);
+		
+		//To Find Data By ID: We use Long Id For Spring JPA
+		System.out.println(repo.findById(2l));
+		System.out.println(repo.findById(3l));
+		
+     	System.out.println(repo.count());
+		
+		// Retrieving and printing all courses
+        System.out.println("All Courses:");
+        repo.findAll().forEach(System.out::println);
+        
+        //Creating By Custom Method
+        System.out.println(repo.findByAuthor("In28Minutes"));
+        System.out.println(repo.findByAuthor(""));
+       
+        System.out.println(repo.findByName("Learn JDBC --SpringJPA"));
+        System.out.println(repo.findByName("Learn Hibernate --SpringJPA"));
+		
+	}
 
 }
--- src/main/java/com/sofi/springboot/learnjdbcjpa/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ src/main/java/com/sofi/springboot/learnjdbcjpa/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,10 +1,14 @@
 package com.sofi.springboot.learnjdbcjpa.course.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sofi.springboot.learnjdbcjpa.course.Course;
 
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
-	
+	//Create Custom Method
+	List<Course> findByAuthor(String author);
+	List<Course> findByName(String name);
 
 }
